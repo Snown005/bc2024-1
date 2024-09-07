1c0dd8541e55259cf8f603ce011ec2bdb93074b6
Nazarii bc2024-1 <nazardovgan005@gmail.com>
2024-09-07 15:29:24 +0300

    file for first lab

---
+++ source.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Lab1<hr>Hello, <b><i>Snown005</i></b>');
+  res.end();
+}).listen(8080);
